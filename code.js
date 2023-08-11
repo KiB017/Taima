fa39a46d5f37243bd1512c9025ca0c2d38c2043b
Kiyoshi Marzo <marzokiyoshi@gmail.com>
2023-08-11 11:53:09 -0400

    timer added

--- code.js
+++ code.js
@@ -1,1 +1,21 @@
-console.log("Hello world");
+var timer;
+var e = document.querySelector('timer');
+var pauseButton = document.querySelector('pause-button');
+var startButton = document.querySelector('start-button');
+
+pauseButton.addEventListener('click', pauseTimer);
+startButton.addEventListener('click', startTimer);
+
+function startTimer()
+{
+    var sec = 0;
+    timer = setInterval(() => {
+        e.innerHTML = '00:' + sec;
+        sec++;
+    }, 1000)
+}
+
+function pauseTimer()
+{
+    clearInterval(timer);
+}
