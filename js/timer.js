31d82a8f59fe70523d3491857e8456733cda1659
Kiyoshi Marzo <marzokiyoshi@gmail.com>
2023-09-25 13:52:24 -0400

    add start/stop function

--- js/timer.js
+++ js/timer.js
@@ -10,13 +10,81 @@ export default class Timer
             seconds: root.querySelector(".timer-part-seconds"),
             control: root.querySelector(".timer_btn_contorl"),
             reset: root.querySelector(".reset-button")
+        };
+
+        this.interval = null;
+        this.remainSeconds = 1500;
+
+
+        this.el.control.addEventListener("click", () => {
+            if(this.interval === null)
+            {
+                this.start();
+            }
+            else
+            {
+                this.stop();
+            }
+        })
+        this.el.reset.addEventListener("click", () => {
+            //add in the code
+        })
+    }
+
+    updateInterfaceTime()
+    {
+        const minutes = Math.floor(this.remainSeconds / 60);
+        const seconds = this.remainSeconds % 60;
+        
+        this.el.minutes.textContent = minutes.toString().padStart(2, "0");
+        this.el.seconds.textContent = seconds.toString().padStart(2, "0");
+    }
+
+    updateInterfaceControls()
+    {
+        if(this.interval === null)
+        {
+            this.el.control.innerHTML = '<span class="material-symbols-outlined">play_circle</span>';
+            this.el.control.classList.add("start-button");
+            this.el.control.classList.remove("stop-button");
         }
+        else
+        {
+            this.el.control.innerHTML = '<span class="material-symbols-outlined">pause_circle</span>';
+            this.el.control.classList.add("stop-button");
+            this.el.control.classList.remove("start-button");
+        }
+    }
+
+    start()
+    {
+        if (this.remainSeconds === 0) return;
+        
+        this.interval = setInterval(() => {
+            this.remainSeconds--;
+            this.updateInterfaceTime();
+
+            if(this.remainSeconds === 0)
+            {
+                this.stop();
+            }
+        }, 1000);
+        this.updateInterfaceControls();
+    }
+
+    stop()
+    {
+        clearInterval(this.interval);
+
+        this.interval = null;
+
+        this.updateInterfaceControls();
     }
 
     static getHTML()
     {
         return `
-            <h3 class="timer-title">Dedication timer</h3>
+            <h3 class="timer-title">Pomodoro timer</h3>
             <div class="timer-container">
                 <span class="timer-part timer-part-minutes">00</span>
                 <span class="timer-part">:</span>
