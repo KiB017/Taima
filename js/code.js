51f3fe34ce61164a4f8280af8e5adc9e3d7dcebd
Kiyoshi Marzo <marzokiyoshi@gmail.com>
2023-10-12 12:13:06 -0400

    fixig the alignment of the elements

--- js/code.js
+++ js/code.js
@@ -18,10 +18,51 @@ function addTask()
         alert("You must write something");
     }
     else
-    {
+    {   
+        
+        
         let li = document.createElement("li");
-        li.innerHTML ='<span class="material-symbols-outlined">check_box_outline_blank</span> ' + inputBox.value;
+        let taskText = document.createElement("span");
+        taskText.classList.add("taskText");
+        taskText.innerText = inputBox.value;
+        let uncheckedBox = document.createElement("span");
+        uncheckedBox.innerHTML = '<span class="material-symbols-outlined darkGreen emptyBox">check_box_outline_blank</span>';
+        let span = document.createElement("span");
+        span.innerHTML = '<span class="material-symbols-outlined darkGreen delete-btn">delete_forever</span>';
+        li.appendChild(uncheckedBox);
+        li.appendChild(taskText);
+        li.appendChild(span);
         listContainer.appendChild(li);
     }
+    inputBox.value = "";
 }
 
+listContainer.addEventListener("click", function(e)
+{
+    let checkedBox = document.createElement("span");
+    checkedBox.innerHTML = '<span class="material-symbols-outlined inactive">check_box</span>';
+    if(e.target.classList.contains("delete-btn"))
+    {
+        li.parentElement.remove();
+        console.log("pup");
+        //saveData();
+    }
+    else if(e.target.classList.contains("taskText") || e.target.classList.contains("emptyBox"))
+    {
+        uncheckedBox.classList.add("inactive");
+        checkedBox.classList.toggle("inactive")
+        taskText.classList.toggle("checked");
+        console.log("pup");
+        //saveData();
+    }
+}, false)
+
+function saveData()
+{
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTasks()
+{
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+//showTasks();
