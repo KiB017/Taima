d6a13c869359d779ef7a9e10809f80ec60cc1549
Kiyoshi Marzo <marzokiyoshi@gmail.com>
2023-10-23 11:33:40 -0400

    adjusting the task item

--- js/code.js
+++ js/code.js
@@ -10,153 +10,50 @@ const taskList = document.querySelector('.task-ist');
 const addBtn = document.querySelector('.add-btn');
 const selectFilter = document.querySelector('.select-filter');
 
-document.addEventListener("DOMContentLoaded", getLocalTasks);
 addBtn.addEventListener("click", addTask);
-taskList.addEventListener("click", deleteCheck);
-selectFilter.addEventListener("click", filterTask);
 
 function addTask(x)
 {
     x.preventDefault();
     const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task");
+
+    const newTask = document.createElement("li");
+    newTask.classList.add("task_text");
+    newTask.innerHTML = inputBox.value;
+
     const deleteBtn = document.createElement("button");
     deleteBtn.innerHTML = '<span class="material-symbols-outlined">delete_forever</span>';
-    deleteBtn.classList.add("delete-btn");
-    taskDiv.appendChild(deleteBtn);
-
-    taskDiv.classList.add("task");
-    const newTask = document.createElement("li");
-    newTask.innerHTML = inputBox.value;
-    newTask.classList.add("task-item");
-    taskDiv.appendChild(newTask);
+    deleteBtn.classList.add("delete_btn");
 
     const completeBtn = document.createElement("button");
     completeBtn.innerHTML = '<span class="material-symbols-outlined">check_box</span>';
-    completeBtn.classList.add("complete-btn");
+    completeBtn.classList.add("checkbox_btn");
+
     taskDiv.appendChild(completeBtn);
+    taskDiv.appendChild(newTask);
+    taskDiv.appendChild(deleteBtn);
 
     taskList.appendChild(taskDiv);
     inputBox.value = "";
 }
 
- function deleteCheck(e)
- {
+
+function deleteCheck(e) {
     const item = e.target;
 
-    if(item.classList [0] === "delete-btn")
-    {
+    if(item.classList[0] === "delete_btn") {
         const task = item.parentElement;
         task.classList.add("slide");
 
-        removeLocalTasks(task);
-        task.addEventListener("transitioned", function() {
+        removeLocalTodos(todo);
+        task.addEventListener("transitionend", function() {
             task.remove();
         });
     }
 
-    if(item.classList[0] === "complete-btn")
-    {
+    if(item.classList[0] === "checkbox_btn") {
         const task = item.parentElement;
         task.classList.toggle("completed");
     }
- }
-
- function filterTask(e)
- {
-    const tasks = taskList.childNodes;
-    tasks.forEach(function(t)
-    {
-        switch(e.target.value)
-        {
-            case "all":
-                t.style.display = "flex";
-                break;
-            case "complete":
-                if(t.classList.contains("complete"))
-                {
-                    t.style.display = "flex";
-                }
-                else
-                {
-                    t.style.display = "none";
-                }
-                break;
-            case "incomplete":
-                if(t.classList.contains("complete"))
-                {
-                    t.style.display = "flex";
-                }
-                else
-                {
-                    t.style.display = "none";
-                }
-                break;
-        }
-    });
- }
-
- function saveLocalTasks(task)
- {
-    let tasks;
-    if(localStorage.getItem("tasks") === null)
-    {
-        tasks = [];
-    }
-    else
-    {
-        tasks = JSON.parse(localStorage.getItem("tasks"));
-    }
-    tasks.push(task);
-    localStorage.setItem("tasks", JSON.stringify(tasks));
- }
-
- function getLocalTasks()
- {
-    let tasks;
-    if(localStorage.getItem("tasks") === null)
-    {
-        tasks = [];
-    }
-    else
-    {
-        tasks = JSON.parse(localStorage.getItem("tasks"));
-    }
-    tasks.forEach(function(task)
-    {
-        const taskDiv = document.createElement("div");
-        const deleteBtn = document.createElement("button");
-        deleteBtn.innerHTML = '<span class="material-symbols-outlined">delete_forever</span>';
-        deleteBtn.classList.add("delete-btn");
-        taskDiv.appendChild(deleteBtn);
-
-        taskDiv.classList.add("task");
-        const newTask = document.createElement("li");
-        newTask.innerHTML = inputBox.value;
-        newTask.classList.add("task-item");
-        taskDiv.appendChild(newTask);
-
-        const completeBtn = document.createElement("button");
-        completeBtn.innerHTML = '<span class="material-symbols-outlined">check_box</span>';
-        completeBtn.classList.add("complete-btn");
-        taskDiv.appendChild(completeBtn);
-
-        taskList.appendChild(taskDiv);
-    });
- }
-
- function removeLocalTasks(task)
- {
-    let tasks;
-    if(localStorage.getItem("tasks") === null)
-    {
-        tasks = [];
-    }
-    else
-    {
-        tasks = JSON.parse(localStorage.getItem("tasks"));
-    }
-
-    const taskIndex = task.children[0].innerText;
-    tasks.splice(tasks.indexOf(taskIndex), 1);
-    localStorage.setItem("tasks", JSON.stringify(tasks))
- }
+}
